6db01e59ba198bf98958b7fdd137dffe3facfe96
Kyle Richey <richey.kyle.i@gmail.com>
2017-11-02 20:11:50 -0700

    following along

---
+++ lab-kyle/lib/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/auth_dev');
+
+const app = require('express')();
+
+app.use(require('../route/auth_routes.js'));
+
+app.use((err,req, res, next) => {
+  console.log(err);
+  res.status(500 || err.statusCode).send(err.message || 'server error');
+  next();
+});
+
+app.listen(process.env.PORT || 5000);
