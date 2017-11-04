6bd83295bcce865d9dc699ebe899daeddec46bac
RyleeAndrews <ryleeandrews73@gmail.com>
2017-11-04 12:13:18 -0700

    first commit added user/sushi model

---
+++ model/sushiSchema.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bluebird').promisifyAll(require('bcrypt'));
+const jwt = require('jsonwebtoken');
+// create user or sushi as I like to call it that contains all the information you need for an individual user
+
+const sushiSchema = new mongoose.Schema({
+  username: {type: String, required: true, unique: true },
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  findHash: {unique:true},
+});
+
+sushiSchema.methods.generateHash = function(password){
+  return bcrypt.hashAsync(password,10)
+  .then( hash => {
+    this.password = hash;
+    return this;
+  });
+};
+
+sushiSchema.methods.comparePassword = function(password){
+  return bcrypt.compareAsync(password,this.password)
+  .then( res => {
+    if(res) return this;
+    throw new Error('password invalid');
+  });
+};
+
+sushiSchema.methods.generateToken = function(){
+  return jwt.sign({id: this._id}, process.env.SECRET || 'changethis');
+};
+
+module.exports = mongoose.model('User', sushiSchema);
