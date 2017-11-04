1ab77a03d9ac2d241051360bcf149bd989a3ab14
Iryna Maslova <maslovai@hotmail.com>
2017-11-04 14:28:18 -0700

    added checking for uniqueness of the name

--- lab16-iryna/routes/auth-routes.js
+++ lab16-iryna/routes/auth-routes.js
@@ -7,14 +7,19 @@ const bodyParser = require('body-parser').json();
 const authRouter = module.exports = require('express').Router();
 
 authRouter.post('/signup', bodyParser, (req, res, next) => {
-    let password = req.body.password;
-    // console.log(password);
-    delete req.body.password;
-    (new User(req.body)).generateHash(password)
+    User.findOne({username:req.body.username})
     .then(user => {
-        // console.log ('USER ', user);
-        user.save()
-        .then(user => res.send(user.generateToken()))
+        if (user) next({statusCode:400, message:'bad request, user exists'});
+        let password = req.body.password;
+        // console.log(password);
+        delete req.body.password;
+        (new User(req.body)).generateHash(password)
+        .then(user => {
+            // console.log ('USER ', user);
+            user.save()
+            .then(user => res.send(user.generateToken()))
+            .catch(next)
+        })
         .catch(next)
     })
     .catch(next)
@@ -23,10 +28,10 @@ authRouter.post('/signup', bodyParser, (req, res, next) => {
 authRouter.get('/signin', basicHTTP, (req, res, next) => {
     User.findOne({username: req.auth.username})
     .then(user => {
-        if(!user) next({statusCode:402, message:'user not found'});
+        if(!user) next({statusCode:401, message:'Unauthorized access'});
         user.comparePasswords(req.auth.password)
         .then(user => res.send(user.generateToken()))
-        .catch(err => next({statusCode:403, message:'incorrect password, your password is guest1234 :)'}))
+        .catch(err => next({statusCode:401, message:'Unauthorized. Check password'}))
     })
     .catch(next)
 })
