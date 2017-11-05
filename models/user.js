c0a3f706c6772ab797822120cd066b65b79b7fe7
Pmookerjee <pmookerjee@gmail.com>
2017-11-04 18:05:37 -0700

    all routes working

--- models/user.js
+++ models/user.js
@@ -7,32 +7,36 @@ const jwt = require('jsonwebtoken');
 const userSchema = new mongoose.Schema({
 
   username: {type: String, required: true, unique: true},
-  password: {type: String, required: true }
+  password: {type: String, required: true },
+  email: {type: String, required: true}
 
 });
 
 userSchema.methods.generateHash = function(password) {
-
   return bcrypt.hashAsync(password, 10)
     .then((hash) => {
+      console.log('in the hash')
       this.password = hash;
       return this;
-    })
-    .catch(next);
+    });
 };
 
 userSchema.methods.verifyPassword = function(password) {
 
   return bcrypt.compareAsync(password, this.password)
-    .then(res => { if(res) return this; })
-    .catch(console.log(err));
+    .then(res => {
+      console.log('res: ', res);
+      if(res) return this;
+      throw new Error('password did not match');
+
+    })
+
+
 
 };
 
 userSchema.methods.generateToken = function() {
-
-  return jwt.sign({ id: this._id }, process.env.SECRET || 'aKJfjk@4927.lk_jfd;pp9');
-
+  return jwt.sign({ id: this._id }, process.env.SECRET || 'aKJfjk4927lkjfdpp9');
 };
 
 module.exports = mongoose.model('User', userSchema);
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,9 +1,45 @@
 'use strict';
 
 const User = require(__dirname + '/../models/user');
-const basicHTTP = require(__dirname + '/../lib/basic-http');
+const basicHTTP = require('../lib/basic-http');
 const jsonParser = require('body-parser').json();
 
 const authRouter = module.exports = require('express').Router();
 
-// authRouter.post()
+authRouter.post('/signup', jsonParser, (req, res, next) => {
+
+  if(!req.body.username || !req.body.password || !req.body.email) next(400);
+
+  User.findOne({ username: req.body.username })
+  .then(userExists => {
+    if(userExists) next(400);
+  }).catch(500);
+
+  const password = req.body.password;
+  delete req.body.password;
+
+  (new User(req.body)).generateHash(password)
+    .then(user => {
+        user.save()
+       .then(user => res.send(user.generateToken()))
+       .catch(401);
+    })
+    .catch(next);
+
+});
+
+
+authRouter.get('/signin', basicHTTP, (req, res, next) => {
+
+  console.log('req.auth.password is ', req.auth.password)
+  User.findOne({username: req.auth.username})
+    .then(user => {
+      console.log('user is ', user);
+      if(!user) { console.log('in the if'); next(401);}
+      user.verifyPassword(req.auth.password)
+        .then(user => res.send(user.generateToken()))
+        .catch(console.log('in the first catch'));
+    })
+    .catch(console.log('in the second catch'));
+
+});
