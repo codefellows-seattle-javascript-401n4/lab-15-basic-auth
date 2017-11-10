e7a76f1e77c5b7bffb3f783186ac8d85263d0c13
Iryna Maslova <maslovai@hotmail.com>
2017-11-09 19:52:05 -0800

    inserted env vars

--- lab16-iryna/lib/server.js
+++ lab16-iryna/lib/server.js
@@ -1,8 +1,9 @@
 'use strict';
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
+const MMONGODB_URI = process.env.MONGODB_URI || 'mongodb://xfvgf7gysd78fgsydfy78sd7f:dfs87df6sd8fsdf87g6dsf87g7xdfg77dx8fg8@ds249325.mlab.com:49325/authdb';
 mongoose.connect(process.env.MONGODB_URI);
 const app = module.exports = require('express')();
 
