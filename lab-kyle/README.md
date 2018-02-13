9231341f89bb2dc4d9eb218ddc1db0c9b5489b67
Kyle Richey <richey.kyle.i@gmail.com>
2018-02-13 09:04:54 -0800

    updated gitignore, readme, eslintrc, and package

---
+++ lab-kyle/README.md
@@ -0,0 +1,7 @@
+Create a .env with PORT and MONGODB_URI
+
+Start mongoDB:
+npm run dbon
+
+Start server:
+npm run start
--- lab-kyle/lib/server.js
+++ lab-kyle/lib/server.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/auth_dev');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/auth_dev', {useMongoClient: true});
 
 const app = require('express')();
 
